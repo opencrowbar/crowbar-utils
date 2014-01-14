58b616d6b42496f85007cdc0ca00d5facb77479a
root <root@cb2dev2ubuntu2>
2014-01-14 08:43:14 -0500

    creates proper bridge - adds interface too

--- docker/crowbar-docker.sh
+++ docker/crowbar-docker.sh
@@ -7,28 +7,28 @@
 # runs container
 
 # argv:
-
 # 1) admin server IP
 # 2) network and netmask docker will run
 # 3) image name to run
 set -x
-ADMIN="192.168.124.10"
-NETWORK="192.168.124.1/24"
-IMAGE="ubuntu-12.04"
+#ADMIN="192.168.124.10"
+#NETWORK="192.168.124.1/24"
+#IMAGE="ubuntu-12.04"
+DEBUG=1
 
 die() { log "$@" >&2; res=1; exit 1; }
-log() { printf "$(date '+%F %T %z'): %s\n" "$@" >&2; }
+log() { if [[ -z "$DEBUG" ]]; then  printf "$(date '+%F %T %z'): %s\n" "$@" >&2 ;fi }
 
 brctl show > /dev/null || die "install brctl, fool."
 
 while (($# > 0)); do
   case $1 in
     --admin) shift
-      ADMIN={$1-"192.168.124.10"};;
+      ADMIN=${1:-"192.168.124.10"};;
     --network) shift
-      NETWORK={$1-"192.168.124.0/24"};;
+      NETWORK=${1:-"192.168.124.0/24"};;
     --image) shift
-      IMAGE={$1-"ubuntu-12.04"};;
+      IMAGE=${1:-"ubuntu-12.04"};;
     *)
       echo "Unknown option $1"
       exit 1;;
@@ -37,16 +37,17 @@ while (($# > 0)); do
 done
 
 # Check for minimum docker version
-DOCKER_VERSION=$(docker version | grep "Server" | awk '{print $2}')
+DOCKER_VERSION=$(docker version | grep "Client" | awk '{print $2}')
 [[ $DOCKER_VERSION =~ ^0+\. ]] && die "Need Docker version >0.7.0.  Please upgrade it."
 DOCKER_MINOR_VERSION=$(echo $DOCKER_VERSION | cut -d'.' -f2)
 [[ $DOCKER_MINOR_VERSION < 7 ]] && die "Need Docker version >0.7.0.  Please upgrade it."
+# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+# curl -s https://get.docker.io/ubuntu/ | sudo sh
 
 log "Admin: ${ADMIN} Network: ${NETWORK} Image: ${IMAGE}"
 
 # is Docker running? get its bridge
 DOCKER_BRIDGE=''
-
 if ps -o command= -C docker
 then
   # docker running with custom bridge? get bridge
@@ -72,10 +73,12 @@ fi
 
 # or, more legibly:
 # get IP address of first hop to $ADMIN
-INTERFACE=$(tracepath ${ADMIN} | head -1 | awk '{ print $2 }' )
+ping -c 1 ${ADMIN} || die "Admin server must be accessible."
+GATEWAY_ADDRESS=$(tracepath ${ADMIN} | head -1 | awk '{ print $2 }' )
+GATEWAY_ADDRESS=$(ip -o -4 a | grep $GATEWAY_ADDRESS | awk '{print $4}')
 # get interface from above IP address
-GATEWAY_TO_ADMIN=$(ip -o -4 a | grep $INTERFACE | awk '{print $2}')
-log "Gateway to Admin: ${GATEWAY_TO_ADMIN}"
+GATEWAY_INTERFACE=$(ip -o -4 a | grep $GATEWAY_ADDRESS | awk '{print $2}')
+log "Gateway to Admin: ${GATEWAY_INTERFACE}"
 
 # does a bridge have this network?
 BRIDGES=($(brctl show | sed -n '2,$ s/^\(\w\+\).*$/\1/p'))
@@ -83,8 +86,7 @@ GOOD_BRIDGE=''
 for BR in ${BRIDGES[@]}; do
   # TODO: this may be wrong.  It'll find the IP address, but  not the network
   # might be better to search for the route
-  if ip -o -4 a show dev ${BR} | grep -q "inet ${NETWORK}" 
-  #if [ $(ip -o -4 a show dev ${BR} | grep -q "inet ${NETWORK}") ]
+  if  ip -o -4 a show dev ${BR} | grep -q "inet ${NETWORK}"   ||  ip -o -4 a show dev ${BR} | grep -q "inet ${ADMIN}"
   then
     echo found ${BR}
     GOOD_BRIDGE=${BR}
@@ -96,9 +98,7 @@ done
 if [ -z "${GOOD_BRIDGE}" ]
 then
   MY_BRIDGE="br${RANDOM}"
-  brctl addbr ${MY_BRIDGE}
-  #ip address add ${NETWORK} dev ${MY_BRIDGE} || die "ip address add error"
-  #ip link set dev ${MY_BRIDGE} up
+  brctl addbr ${MY_BRIDGE} || die "Cant add bridge: $MY_BRIDGE"
   GOOD_BRIDGE=${MY_BRIDGE}
 fi
 log "Using bridge ${GOOD_BRIDGE}"
@@ -106,31 +106,39 @@ log "Using bridge ${GOOD_BRIDGE}"
 # setup docker to use the good bridge
 if [[ ${DOCKER_BRIDGE} != ${GOOD_BRIDGE} ]]
 then
-  stop docker
+  stop docker 2>&1 > /dev/null
   # change an existing docker config
-  grep '\-b=' /etc/default/docker && perl -pi -e "s/-b=\w+/-b=${GOOD_BRIDGE}/" /etc/default/docker
+  if grep -q '^[^#].*\-b=' /etc/default/docker 
+  then
+    perl -pi -e "s/-b=\w+/-b=${GOOD_BRIDGE}/" /etc/default/docker
   # or add to a file without a config
-  grep DOCKER_OPTS /etc/default/docker || echo "DOCKER_OPTS=\"-b=${GOOD_BRIDGE}\"" >> /etc/default/docker
-  start docker
+  elif grep -q ^DOCKER_OPTS /etc/default/docker 
+  then
+    sed -i "s/^DOCKER_OPTS=\"\(.*\)\"/DOCKER_OPTS=\"\1 -b=${GOOD_BRIDGE}\"/" /etc/default/docker
+  else
+    echo "DOCKER_OPTS=\"-b=${GOOD_BRIDGE}\"" >> /etc/default/docker
+  fi
 fi
 
 # are containers running?
 
 echo "You've gotta stop your containers to reconfigure Docker."
 
-# check network
-## can I ping the admin server?  Does it matter?
-
+# Add NIC to Bridge
 # remove IP address from the gateway NIC
+ip addr del ${ADMIN} dev ${GATEWAY_INTERFACE}
 # add IP address to the bridge
-# add the NIC to the bridge
+ip address add ${GATEWAY_ADDRESS} dev ${GOOD_BRIDGE} || die "ip address add error"
+# add the Gateway NIC to the bridge
+#brctl addif ${GOOD_BRIDGE} ${GATEWAY_INTERFACE}
+ip link set ${GATEWAY_INTERFACE} master ${GOOD_BRIDGE}
+ip link set ${GOOD_BRIDGE} up
 
+start docker
 
 # make a container and network out of it.
 
-# run image to create Container 
-
-
+# run image to create Container
 
 get_ips_of_bridge() {
   local bridge
