00974c0aa668b6350e14940924d571e7f7ba555a
Judd Maltin <judd@newgoliath.com>
2014-03-03 15:52:51 -0500

    setup upstream and personal branches - person IS ORIGIN

--- vagrant-opencrowbar/chef/cookbooks/cb2/recipes/crowbar-1-source-and-build-prep.rb
+++ vagrant-opencrowbar/chef/cookbooks/cb2/recipes/crowbar-1-source-and-build-prep.rb
@@ -10,8 +10,7 @@ when "ubuntu"
   package "debhelper"
   package "cabextract"
   # testing packages
-  package "erlang-base"
-  package "erlang-inets"
+  package "erlang-base" package "erlang-inets"
 #  package "ruby-dev" 
 #  package "ruby-bundler" 
   package "libsqlite3-dev" 
@@ -57,32 +56,48 @@ template "/home/#{node.props.guest_username}/.gitconfig" do
  	})
 end
 
-# old crowbar
-# setup .build-crowbar.conf
-#template "/home/#{node.props.guest_username}/.build-crowbar.conf" do
-#	source "buildcrowbarconf.erb"
-#	mode 0777
-#	owner node.props.guest_username
-#	group node.props.guest_username
-#	variables ({
-#		:github_id => node.props.github_id,
-#		:iso_library => node.props.crowbar_iso_library,
-#		:iso_dest => node.props.crowbar_iso_dest,
-#		:cache_dir => node.props.crowbar_build_cache
-#	})
-#end
+# add some remotes
+node.props.attribute?('github_extra_remotes') &&
+node.props[:github_extra_remotes].each do | remote_name, remote_url |
+  log "Adding extra git remote repositories"
+  execute "set remote #{remote_name} with url #{remote_url}" do
+    user node.props.guest_username
+    cwd "/home/#{node.props.guest_username}/opencrowbar/core/"
+    command "git remote add #{remote_name} #{remote_url}; git fetch #{remote_name}"
+    action :run
+    not_if "git remote -v #{remote_name}", 
+      :user => node.props.guest_username, 
+      :cwd => "/home/#{node.props.guest_username}/opencrowbar/core/"
+    end
+end
 
 
 # grab the crowbar repo
 log ( "Cloning Crowbar repo - takes forever" )
 envhash = { "LOGNAME" => "#{node.props.guest_username}", 'HOME' => "/home/#{node.props.guest_username}" }
 
-execute "git clone crowbar" do
+opencrowbar_dir = ENV['HOME'] + 'opencrowbar'
+
+directory opencrowbar_dir do
+  owner node.props.guest_username
+  action :create
+end
+
+execute "git clone opencrowbar personal" do
 	user "#{node[:props][:guest_username]}"
 	group "#{node[:props][:guest_username]}"
-	cwd "/home/#{node[:props][:guest_username]}"
+	cwd opencrowbar_dir
 	command "git clone #{node[:props][:github_repo]}"
-	creates "/home/#{node[:props][:guest_username]}/core/"
+	creates opencrowbar_dir
+	environment envhash
+end
+
+execute "git pull opencrowbar master " do
+	user "#{node[:props][:guest_username]}"
+	group "#{node[:props][:guest_username]}"
+	cwd opencrowbar_dir
+	command "git pull upstream"
+	creates opencrowbar_dir
 	environment envhash
 end
 
